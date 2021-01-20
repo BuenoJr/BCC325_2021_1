4661d1f14fbae43c90b3b61fb187ed9db756b56a
Valmir Rodrigues Bueno Júnior <valmir.bueno@aluno.ufop.edu.br>
2021-01-20 11:02:05 -0300

    Add Agent, Environment

---
+++ Atividade Pratica Semana 1 e 2/definitions.py
@@ -0,0 +1,13 @@
+class Environment:
+    def initial_percepts ( self ):
+        raise NotImplementedError ('initial_percepts')
+    
+    def signal ( self , action ) :
+        raise NotImplementedError ('signal')
+
+class Agent:
+    def __init__(self, env):
+        self.env = env
+
+    def act(self):
+        raise NotImplementedError('act')
