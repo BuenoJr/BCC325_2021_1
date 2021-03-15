7bde2b13878d2246e39655141200b1ea1f66fee7
Valmir Rodrigues Bueno Júnior <valmir.bueno@aluno.ufop.edu.br>
2021-03-15 09:35:22 -0300

    bottom_up, top_down e explain

--- logical_agents.py
+++ logical_agents.py
@@ -1,6 +1,6 @@
 class LogicalAgent():
 
-    def __init__(self,KB):
+    def __init__(self, KB):
         self.KB = KB
 
     # TODO
@@ -10,13 +10,36 @@ class LogicalAgent():
         Returns:
             A list with all the logical consequences of KB
         '''
-        pass
+        consequenceList = []
+
+        for ask in self.KB.askables:
+            if ask not in consequenceList:
+                answer = True if input(
+                    'Is {} true / (yes/no):'.format(str(ask))) == 'yes' else False
+
+                if answer:
+                    consequenceList.append(ask.atom)
+
+        while True:
+            initialSize = len(consequenceList)
+            clauses = self.KB.clauses
+            for clause in clauses:
+                if clause.head not in consequenceList:
+                    if not clause.body:
+                        consequenceList.append(clause.head)
+                    else:
+                        if all(map(lambda atom: atom in consequenceList, clause.body)):
+                            consequenceList.append(clause.head)
+
+            if initialSize == len(consequenceList):
+                break
+        return consequenceList
 
     # TODO
-    def top_down(self,query):
+    def top_down(self, query):
         '''Implements the top down proof strategy. Given a query (the atom that it wants to prove) 
         it returns True if the query is a consequence of the knowledge base. 
-        
+
         Args:
             querry: The atom that should be proved
 
@@ -24,18 +47,70 @@ class LogicalAgent():
             True if the query is a logical consequence of KB, False otherwise
 
         '''
-        
-        pass
-    
+
+        questions = query
+        askable = []
+        askableTrue = []
+        for ask in self.KB.askables:
+            askable.append(ask.atom)
+
+        while questions != []:
+            atom = questions.pop(0)
+            premise = self.KB.clauses_for_atom(atom)
+            if len(premise) > 0:
+                questions = premise[0].body + questions
+            else:
+                if atom not in askableTrue and atom in askable:
+                    answer = True if input(
+                        'Is {} true / (yes/no):'.format(str(atom))) == 'yes' else False
+                    if answer:
+                        askable.remove(atom)
+                        askableTrue.append(atom)
+                    else:
+                        return False
+                else:
+                    return False
+        return True
+
     # TODO
-    def explain(self,g):
+
+    def explain(self, g):
         '''Implements the process of abductions. It tries to explain the atoms  in the list g using
          the assumable in KB.
 
         Args:
             g: A set of atoms that should be explained
-        
+
         Returns:
             A list of explanation for the atoms in g
         '''
-        pass
+        assumable = []
+        consequenceList = []
+
+        for assumed in self.KB.assumable:
+            assumable.append(assumed.atom)
+
+        for initial in g:
+            if initial in assumable:
+                consequenceList.append(initial)
+
+        while g != []:
+            atom = g.pop(0)
+            clauses = self.KB.clauses_for_atom(atom)
+
+            for premissa in clauses:
+                for body in premissa.body:
+                    if body in assumable:
+                        clausesFalses = self.KB.clauses_for_atom('false')
+                        add = True
+                        for clausesFalse in clausesFalses:
+                            if body in clausesFalse.body and [a in consequenceList for a in clausesFalse.body]:
+                                add = False
+
+                        if add:
+                            consequenceList.append(body)
+                    else:
+                        g = [body] + g
+
+        consequenceList = list(set(consequenceList))
+        return consequenceList
--- logical_agents_simulation.py
+++ logical_agents_simulation.py
@@ -2,21 +2,43 @@ from knowledge_base import *
 from logical_agents import LogicalAgent
 
 
-kb = KB([Clause('a',['b','c']),
-        Clause('b',['g','e']),
-        Clause('b',['d','e']),
-        Clause('c',['e']),
-        Askable('d'),
-        Clause('e'),
-        Clause('f',['a','g']) ])
+kb = KB([Clause('a', ['b', 'c']),
+         Clause('b', ['g', 'e']),
+         Clause('b', ['d', 'e']),
+         Clause('c', ['e']),
+         Askable('d'),
+         Askable('k'),
+         Clause('e'),
+         Clause('f', ['a', 'g'])])
 
-print(kb)
+kb2 = KB([Clause('a', ['b', 'c']),
+          Clause('b', ['e']),
+          Clause('b', ['d']),
+          Clause('c'),
+          Clause('d', ['h']),
+          Clause('e'),
+          Clause('g', ['a', 'b', 'e']),
+          Clause('f', ['h', 'b'])])
 
-ag = LogicalAgent(kb)
+# print(kb2)
+#[print(a) for a in kb.askables]
+#[print(a) for a in kb.clauses]
+#[print(a) for a in kb.c_for_a]
+# #[print(a) for a in kb.c_for_a.items()]
+#[print(c) for c in kb.clauses_for_atom('a')]
+#[[print(c) for c in a[1]] for a in kb.c_for_a.items()]
+
+#[print(a) for a in kb.statements]
+
+# for a in kb.askables:
+# aval = True if input(
+# 'Is {} true / (yes/no):'.format(str(a))) == 'yes' else False
+
+ag = LogicalAgent(kb2)
 
 # Derive all the logical consequences of KB
-consequences = ag.bottom_up()
-print(consequences)
+#consequences = ag.bottom_up()
+# print(consequences)
 
 # Prove 'a'
-print(ag.top_down(['a']))
+print(ag.top_down(['g']))
